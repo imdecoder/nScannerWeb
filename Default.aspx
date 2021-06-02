ee4fc2e028c67598327ffd3ca492c5a92c9ae0da
imdecoder <imdecoder@mail.com>
2021-06-02 18:31:36 +0300

    Initial commit

---
+++ Default.aspx
@@ -0,0 +1,87 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Default.aspx.cs" Inherits="nScanner.Default" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title>nScanner</title>
+
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+</head>
+<body>
+    <div class="video-wrap">
+        <video id="video" playsinline autoplay></video>
+    </div>
+    <div class="controller">
+        <button id="snap">
+            Capture
+        </button>
+        <button id="decode">
+            Decode
+        </button>
+    </div>
+    <canvas id="canvas" width="200" height="200"></canvas>
+    <image id="image"></image>
+
+    <script>
+        'use strict';
+
+        const video = document.getElementById('video');
+        const canvas = document.getElementById('canvas');
+        const image = document.getElementById('image');
+        const snap = document.getElementById('snap');
+        const decode = document.getElementById('decode');
+        const errorMsg = document.getElementById('errorMsg');
+
+        const constraints = {
+            video: {
+                width: 200,
+                height: 200
+            }
+        }
+
+        async function init() {
+            try {
+                const stream = await navigator.mediaDevices.getUserMedia(constraints);
+                handleSuccess(stream);
+            }
+            catch (e) {
+                errorMsg.innerHTML = `navigator.getUserMedia.error: ${e.toString()}`;
+            }
+        }
+
+        function handleSuccess(stream) {
+            window.stream = stream;
+            video.srcObject = stream;
+        }
+
+        init();
+
+        var context = canvas.getContext('2d');
+
+        snap.addEventListener('click', function () {
+            context.drawImage(video, 0, 0, 200, 200);
+        });
+
+        decode.addEventListener('click', function () {
+            image.src = canvas.toDataURL();
+
+            var imageSrc = image.src;
+
+            $.ajax({
+                type: 'POST',
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+                url: 'Default.aspx/ImageDecoder',
+                data: '{imageSrc: "' + imageSrc + '" }',
+                success: function (result) {
+                    alert(result.d);
+                },
+                error: function (xhr, status, error) {
+                    alert(xhr.responseText);
+                    //alert('Hata! Sunucuya ulaşılamıyor.');
+                }
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,94 @@
+﻿using ZXing;
+using OpenCvSharp;
+using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.IO;
+using System.Text.RegularExpressions;
+using System.Web.Services;
+using ZXing.Common;
+using OpenCvSharp.CPlusPlus;
+using OpenCvSharp.Extensions;
+using System.Collections.Generic;
+
+namespace nScanner
+{
+    public partial class Default : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+        }
+
+        [WebMethod]
+        public static string ImageDecoder(string imageSrc)
+        {
+            string imageURL = imageSrc.Replace("data:image/png;base64,", "");
+
+            Bitmap bmpFromString = Base64StringToBitmap(imageURL);
+            var barcodeBitmap = new Bitmap(bmpFromString);
+
+            var source = new BitmapLuminanceSource(barcodeBitmap);
+
+            // using http://zxingnet.codeplex.com/
+            // PM> Install-Package ZXing.Net
+            var reader = new BarcodeReader(null, null, ls => new GlobalHistogramBinarizer(ls))
+            {
+                AutoRotate = true,
+                TryInverted = true,
+                Options = new DecodingOptions
+                {
+                    TryHarder = true,
+                    PureBarcode = true,
+                    PossibleFormats = new List<BarcodeFormat>
+                    {
+                        BarcodeFormat.CODE_128,
+                        BarcodeFormat.EAN_8,
+                        BarcodeFormat.CODE_39,
+                        BarcodeFormat.UPC_A
+                    }
+                }
+            };
+
+            var newhint = new KeyValuePair<DecodeHintType, object>(DecodeHintType.ALLOWED_EAN_EXTENSIONS, new Object());
+            reader.Options.Hints.Add(newhint);
+
+            var result = reader.Decode(source);
+            if (result == null)
+            {
+                return string.Empty;
+            }
+
+            var writer = new BarcodeWriter
+            {
+                Format = result.BarcodeFormat,
+                Options = { Width = 200, Height = 50, Margin = 4 },
+                Renderer = new ZXing.Rendering.BitmapRenderer()
+            };
+            
+            var barcodeImage = writer.Write(result.Text);
+            Cv2.ImShow("BarcodeWriter", barcodeImage.ToMat());
+
+            return result.Text;
+        }
+
+        public static Bitmap Base64StringToBitmap(string base64String)
+        {
+            Bitmap bmpReturn = null;
+
+            //Convert Base64 string to byte[]
+            byte[] byteBuffer = Convert.FromBase64String(base64String);
+
+            MemoryStream memoryStream = new MemoryStream(byteBuffer);
+
+            memoryStream.Position = 0;
+
+            bmpReturn = (Bitmap)Image.FromStream(memoryStream);
+
+            memoryStream.Close();
+            memoryStream = null;
+            byteBuffer = null;
+
+            return bmpReturn;
+        }
+    }
+}
