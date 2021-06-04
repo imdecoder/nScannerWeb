b8dc185572e1e15607e1fa8e5a4f788b84f38d63
imdecoder <mail@eminarifpirinc.com.tr>
2021-06-04 13:11:39 +0300

    Added scanner

--- Default.aspx.cs
+++ Default.aspx.cs
@@ -1,15 +1,8 @@
-﻿using ZXing;
-using OpenCvSharp;
-using System;
-using System.ComponentModel;
+﻿using System;
 using System.Drawing;
 using System.IO;
-using System.Text.RegularExpressions;
 using System.Web.Services;
-using ZXing.Common;
-using OpenCvSharp.CPlusPlus;
-using OpenCvSharp.Extensions;
-using System.Collections.Generic;
+using ZXing;
 
 namespace nScanner
 {
@@ -22,73 +15,60 @@ namespace nScanner
         [WebMethod]
         public static string ImageDecoder(string imageSrc)
         {
-            string imageURL = imageSrc.Replace("data:image/png;base64,", "");
+            string base64 = imageSrc.Replace("data:image/png;base64,", "");
 
-            Bitmap bmpFromString = Base64StringToBitmap(imageURL);
-            var barcodeBitmap = new Bitmap(bmpFromString);
+            //SaveByteArrayAsImage(@"C:/nScanner/output.png", base64);
 
-            var source = new BitmapLuminanceSource(barcodeBitmap);
+            Bitmap bmp = Base64StringToBitmap(base64);
+            BarcodeReader reader = new BarcodeReader();
 
-            // using http://zxingnet.codeplex.com/
-            // PM> Install-Package ZXing.Net
-            var reader = new BarcodeReader(null, null, ls => new GlobalHistogramBinarizer(ls))
+            try
             {
-                AutoRotate = true,
-                TryInverted = true,
-                Options = new DecodingOptions
+                Result result = reader.Decode(bmp);
+
+                if (result == null)
                 {
-                    TryHarder = true,
-                    PureBarcode = true,
-                    PossibleFormats = new List<BarcodeFormat>
-                    {
-                        BarcodeFormat.CODE_128,
-                        BarcodeFormat.EAN_8,
-                        BarcodeFormat.CODE_39,
-                        BarcodeFormat.UPC_A
-                    }
+                    return "error";
                 }
-            };
 
-            var newhint = new KeyValuePair<DecodeHintType, object>(DecodeHintType.ALLOWED_EAN_EXTENSIONS, new Object());
-            reader.Options.Hints.Add(newhint);
-
-            var result = reader.Decode(source);
-            if (result == null)
-            {
-                return string.Empty;
+                return result.Text;
             }
-
-            var writer = new BarcodeWriter
+            catch (Exception)
             {
-                Format = result.BarcodeFormat,
-                Options = { Width = 200, Height = 50, Margin = 4 },
-                Renderer = new ZXing.Rendering.BitmapRenderer()
-            };
-            
-            var barcodeImage = writer.Write(result.Text);
-            Cv2.ImShow("BarcodeWriter", barcodeImage.ToMat());
-
-            return result.Text;
+                return "error";
+            }
         }
 
         public static Bitmap Base64StringToBitmap(string base64String)
         {
             Bitmap bmpReturn = null;
 
-            //Convert Base64 string to byte[]
             byte[] byteBuffer = Convert.FromBase64String(base64String);
-
             MemoryStream memoryStream = new MemoryStream(byteBuffer);
 
             memoryStream.Position = 0;
 
-            bmpReturn = (Bitmap)Image.FromStream(memoryStream);
+            bmpReturn = (Bitmap)Bitmap.FromStream(memoryStream);
 
             memoryStream.Close();
             memoryStream = null;
             byteBuffer = null;
 
             return bmpReturn;
+        }
+
+        private static void SaveByteArrayAsImage(string fullOutputPath, string base64String)
+        {
+            byte[] bytes = Convert.FromBase64String(base64String);
+
+            Image image;
+            
+            using (MemoryStream ms = new MemoryStream(bytes))
+            {
+                image = Image.FromStream(ms);
+            }
+
+            image.Save(fullOutputPath, System.Drawing.Imaging.ImageFormat.Png);
         }
     }
 }
